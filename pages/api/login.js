7cf3f9b648a22d6f7b23426947017339a858021f
AlexCat <eiseman.alex@gmail.com>
2022-08-02 14:37:41 -0500

    test Configure Default Login Routes

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,5 +1,24 @@
 import auth0 from '../../lib/auth0'
 
+var axios = require("axios").default;
+
+var options = {
+  method: 'PATCH',
+  url: 'https://dev-pg11uqii.us.auth0.com/api/v2/clients/8BnkSyAQZVi2HEdubL2ykRHkQ74em6hR',
+  headers: {
+    'content-type': 'application/json',
+    authorization: 'Bearer API2_ACCESS_TOKEN',
+    'cache-control': 'no-cache'
+  },
+  data: {initiate_login_uri: 'https://human-coop.vercel.app/api/login'}
+};
+
+axios.request(options).then(function (response) {
+  console.log(response.data);
+}).catch(function (error) {
+  console.error(error);
+});
+
 export default async function login(req, res) {
   try {
     await auth0.handleLogin(req, res)
