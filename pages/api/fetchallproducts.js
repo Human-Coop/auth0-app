c46b376d498366d34ba4e0f26a26ad449988c796
Regina Sullivan <eiseman.alex@gmail.com>
2023-11-08 14:11:47 -0600

    test product load

--- pages/api/fetchallproducts.js
+++ pages/api/fetchallproducts.js
@@ -1,27 +1,8 @@
-// todo: pull from json file in products folder
-var products = [{
-    "id":1,
-    "name":"test",
-    "img":"../products/images/test.jpg",
-    "price":"$99",
-    "stock":1,
-    "details":"test"
-    },
-    {
-      "id":2,
-      "name":"test2",
-      "img":"../products/images/test.jpg",
-      "price":"$99",
-      "stock":2,
-      "details":"test2"
-      }]
+import products from './products.json' assert { type: 'json' };
 // todo: write handleProductlist function -- eventually products might live in db
 export default async function fetchallproducts(req, res) {
     try {
-      fetch('./products.json')
-      .then((response) => {return response.json()})
-      .then((json) => console.log(json));
-        //return products
+        return products
         //await handleProductlist(req, res)
     } catch (error) {
       console.error(error)
