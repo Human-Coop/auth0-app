f39ebc84d1a99bbe9ae1422312dbdfc5ae66c57e
Regina Sullivan <eiseman.alex@gmail.com>
2023-11-08 15:03:30 -0600

    test product load

--- pages/products.js
+++ pages/products.js
@@ -1,23 +1,29 @@
 import Layout from '../components/layout'
 import { useFetchUser } from '../lib/user'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+// todo: don't be so hacky
 // import { useFetchProducts } from '../lib/productlist'
 import products from './api/allproducts.json' assert { type: 'json' };
 
 // // todo: write addcart function
-// function ProductCard({ product }) {
-//     return (
-//       <>
-//         <div>
-//             <h1>{product.name}</h1>
-//             <img src={product.img} />
-//             <h3>Price: {product.price}</h3>
-//             <h4>Stock: {product.stock}</h4>
-//             <p>Details: {product.details}</p>
-//             <button onclick={addcart(product.id)}>Add to cart</button>
-//         </div>
-//       </>
-//     )
-//   }
+function ProductCard(product) {
+  return (
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={product.img} />
+      <Card.Body>
+        <Card.Title>{product.name}</Card.Title>
+        <Card.Text>
+          <h3>Price: {product.price}</h3>
+          <h4>Stock: {product.stock}</h4>
+          <p>Details: {product.details}</p>
+        </Card.Text>
+        {/* <Button variant="primary" onclick={addcart(product.id)}>Add to Cart</Button> */}
+      </Card.Body>
+    </Card>
+  );
+}
 
 function Products() {
   const { user, loading } = useFetchUser()
@@ -31,14 +37,7 @@ function Products() {
         { !productlist ? <>Loading...</> :
         productlist.map((product) => (
             <li>
-                <div>
-                    <h1>{product.name}</h1>
-                    <img src={product.img} />
-                    <h3>Price: {product.price}</h3>
-                    <h4>Stock: {product.stock}</h4>
-                    <p>Details: {product.details}</p>
-                    {/* <button onclick={addcart(product.id)}>Add to cart</button> */}
-                </div>
+              {ProductCard(product)}
             </li>
           ))
         }
